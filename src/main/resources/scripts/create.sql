c8e6bb1e3752eaa0c2b3abbce7ceed4bfbc030ac
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2023-12-26 14:01:28 -0300

    Docs: modificando relacionamentos no scrpt do banco

--- src/main/resources/scripts/create.sql
+++ src/main/resources/scripts/create.sql
@@ -3,25 +3,27 @@ CREATE DATABASE TCS;
 USE TCS;
 
 CREATE TABLE mensagem (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     conteudo VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE input (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    conteudo VARCHAR(100) NOT NULL
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    conteudo VARCHAR(100) NOT NULL,
+    id_mensagem_anterior BIGINT,
+    id_mensagem_sucessora BIGINT,
+    FOREIGN KEY (id_mensagem_anterior) REFERENCES mensagem(id),
+    FOREIGN KEY (id_mensagem_sucessora) REFERENCES mensagem(id)
 );
 
-CREATE TABLE arvore_mensagem (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_mensagem INT,
-    id_input INT,
-    FOREIGN KEY (id_mensagem) REFERENCES mensagem(id),
-    FOREIGN KEY (id_input) REFERENCES input(id)
+CREATE TABLE graph_mensagem (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_mensagem_root BIGINT NOT NULL,
+    FOREIGN KEY (id_mensagem_root) REFERENCES mensagem(id)
 );
 
 CREATE TABLE usuario (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE,
     senha VARCHAR(255),
@@ -29,24 +31,24 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE setor (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(255) NOT NULL,
-    id_arvore_mensagem INT,
-    FOREIGN KEY (id_arvore_mensagem) REFERENCES arvore_mensagem(id)
+    id_graph_mensagem BIGINT,
+    FOREIGN KEY (id_graph_mensagem) REFERENCES graph_mensagem(id)
 );
 
 CREATE TABLE usuario_setor (
-    id_usuario INT,
-    id_setor INT,
+    id_usuario BIGINT,
+    id_setor BIGINT,
     administrador TINYINT NOT NULL DEFAULT 0,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id),
     FOREIGN KEY (id_setor) REFERENCES setor(id)
 );
 
 create table contato(
-	id int not null auto_increment primary key
-	,id_usuario int not null
+	id BIGINT not null auto_increment primary key
+	,id_usuario BIGINT not null
 	,nome varchar(255) not null
 	,numero char(11) unique
 	,foreign key (id_usuario) references usuario(id)
