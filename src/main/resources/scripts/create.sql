7bdac82004c266017c5f9acf75817ee7cba96a61
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2024-01-29 18:36:28 -0300

    Chore: Alteração no banco de dados

--- src/main/resources/scripts/create.sql
+++ src/main/resources/scripts/create.sql
@@ -2,26 +2,6 @@ DROP DATABASE IF EXISTS TCS;
 CREATE DATABASE TCS;
 USE TCS;
 
-CREATE TABLE mensagem (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    conteudo VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE input (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    conteudo VARCHAR(100) NOT NULL,
-    id_mensagem_anterior BIGINT,
-    id_mensagem_sucessora BIGINT,
-    FOREIGN KEY (id_mensagem_anterior) REFERENCES mensagem(id),
-    FOREIGN KEY (id_mensagem_sucessora) REFERENCES mensagem(id)
-);
-
-CREATE TABLE graph_mensagem (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_mensagem_root BIGINT NOT NULL,
-    FOREIGN KEY (id_mensagem_root) REFERENCES mensagem(id)
-);
-
 CREATE TABLE usuario (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -33,9 +13,7 @@ CREATE TABLE usuario (
 CREATE TABLE setor (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    descricao VARCHAR(255) NOT NULL,
-    id_graph_mensagem BIGINT,
-    FOREIGN KEY (id_graph_mensagem) REFERENCES graph_mensagem(id)
+    descricao VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE usuario_setor (
@@ -52,4 +30,20 @@ create table contato(
 	,nome varchar(255) not null
 	,numero char(11) unique
 	,foreign key (id_usuario) references usuario(id)
+);
+
+CREATE TABLE mensagem (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    conteudo VARCHAR(255) NOT NULL,
+    id_setor BIGINT not null,
+    FOREIGN KEY (id_setor) references setor(id)
+);
+
+CREATE TABLE input (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    conteudo VARCHAR(100) NOT NULL,
+    id_mensagem_anterior BIGINT,
+    id_mensagem_sucessora BIGINT,
+    FOREIGN KEY (id_mensagem_anterior) REFERENCES mensagem(id),
+    FOREIGN KEY (id_mensagem_sucessora) REFERENCES mensagem(id)
 );
