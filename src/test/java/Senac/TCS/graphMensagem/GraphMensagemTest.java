d0e347c8d648eab31aa9c721bde51ed4b2c3dfd9
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2024-01-29 18:37:21 -0300

    refactor: remoção da entidade GraphMensagem

--- src/test/java/Senac/TCS/graphMensagem/GraphMensagemTest.java
+++ src/test/java/Senac/TCS/graphMensagem/GraphMensagemTest.java
@@ -1,29 +1,6 @@
 package Senac.TCS.graphMensagem;
 
-import Senac.TCS.model.entity.GraphMensagem;
-import Senac.TCS.model.entity.Input;
-import Senac.TCS.model.entity.Mensagem;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class GraphMensagemTest {
-    @Test
-    public void testAdicionarInputNaMensagemRoot() {
-        GraphMensagem graphMensagem = new GraphMensagem();
 
-        String conteudoInput = "Conteúdo do input";
-        graphMensagem.adicionarInputNaMensagemRoot(conteudoInput);
-
-        Mensagem root = graphMensagem.getRoot();
-        assertNotNull(root);
-        assertNotNull(root.getInputs());
-        assertFalse(root.getInputs().isEmpty());
-
-        Input inputAdicionado = root.getInputs().get(0);
-        assertNotNull(inputAdicionado);
-        assertEquals(conteudoInput, inputAdicionado.getConteudo());
-        assertEquals(root, inputAdicionado.getMensagemAnterior());
-        assertNull(inputAdicionado.getMensagemSucessora());
-    }
 }
 
