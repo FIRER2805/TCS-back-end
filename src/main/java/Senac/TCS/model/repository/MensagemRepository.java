eda6b2c4e662aa2d95f7ffbf93705cb0bd8f22d4
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2024-02-20 23:58:20 -0300

    optmize: otimiza query para buscar mensagem root

--- src/main/java/Senac/TCS/model/repository/MensagemRepository.java
+++ src/main/java/Senac/TCS/model/repository/MensagemRepository.java
@@ -1,21 +1,21 @@
 package Senac.TCS.model.repository;
 
 import Senac.TCS.model.entity.Mensagem;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MensagemRepository extends CrudRepository<Mensagem,Long> {
 
-    @Query(value="select m.* from mensagem m\n" +
-            "join setor s on\n" +
-            "s.id = m.id_setor\n" +
-            "join mensagem_input mi on\n" +
-            "mi.id_mensagem_pai = m.id\n" +
+    @Query(value="select m.* from mensagem m \n" +
+            "join setor s on \n" +
+            "s.id = m.id_setor \n" +
             "where s.id = :idSetor \n" +
-            "AND not exists(select 1 from mensagem_input mi2  where mi2.id_mensagem_filha = mi.id);",
+            "AND not exists(select 1 from mensagem_input mi  where mi.id_mensagem_filha = m.id);",
     nativeQuery = true)
     Optional<Mensagem> obterMensagemRoot(@Param("idSetor") Long idSetor);
 
