142a9ca61b0f7531ddc5b9b606d33e4b610855e9
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2024-02-21 16:21:30 -0300

    refactor: alterando a estrutura do banco e deletando classes desnecessarias

--- src/main/java/Senac/TCS/model/repository/InputRepository.java
+++
@@ -1,9 +0,0 @@
-package Senac.TCS.model.repository;
-
-import Senac.TCS.model.entity.Input;
-import Senac.TCS.model.entity.Mensagem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InputRepository extends CrudRepository<Input,Long> {
-    
-}
