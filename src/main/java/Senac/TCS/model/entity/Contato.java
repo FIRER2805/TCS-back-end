79306595c11f1bd5bc7398e587a07ab9653dfa12
Milhouse-1906 <gabriellopesgoncalves189@gmail.com>
2024-03-12 14:11:25 -0300

    Fix : Get e set de usuario adicionados na entidade contato

--- src/main/java/Senac/TCS/model/entity/Contato.java
+++ src/main/java/Senac/TCS/model/entity/Contato.java
@@ -43,27 +43,5 @@ public class Contato {
 		this.id = id;
 	}
 
-//	public Usuario getUsuario() {
-//		return usuario;
-//	}
-//
-//	public void setUsuario(Usuario usuario) {
-//		this.usuario = usuario;
-//	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
+	
 }
